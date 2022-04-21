b58d7ce9b448050bcb9255121752ebbae93cfc5c
Marius Dill <dill_marius@hotmail.de>
2022-04-21 14:31:02 +0200

    Initital Release

---
+++ Postfach_erstellen.ps1
@@ -0,0 +1,18 @@
+﻿$UserCredential = Get-Credential
+
+$Username = $args[0]
+
+#$Username = Read-Host "Benutzername"
+
+$ParamsConnection = @{
+    ConfigurationName   = "Microsoft.Exchange"
+    ConnectionUri       = "http://exchange2019/PowerShell/"
+    Authentication      = "Kerberos"
+    Credential          = $UserCredential
+}
+$Session = New-PSSession @ParamsConnection    
+Import-PSSession $Session -CommandName Enable-Mailbox -AllowClobber | Out-Null
+
+Enable-Mailbox -Identity $Username@bkh-lohr.local
+
+Remove-PSSession $Session
