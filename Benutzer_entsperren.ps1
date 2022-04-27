78991fd01b3f97d17b5ee9044e53fb10db485af3
Marius Dill <dill_marius@hotmail.de>
2022-04-27 15:07:02 +0200

    Remote Execute Script on DC02

--- Benutzer_entsperren.ps1
+++ Benutzer_entsperren.ps1
@@ -1,1 +1,15 @@
-﻿Search-ADAccount -LockedOut | Out-GridView -PassThru | Unlock-ADAccount
+﻿# Login
+$TXT = "\\bkh-lohr.local\dfs\abteilung$\EDV\Soft_Hardware\_Skripts\EasyJob\scripts\Import\Passwort_Serveradmin.txt"
+$Username = "admmarius"
+$Password =  Get-Content $TXT | ConvertTo-SecureString -AsPlainText -Force
+$UserCredential = New-Object System.Management.Automation.PsCredential $Username, $Password
+
+# Remotepowershell verbinden
+$Session = New-PSSession -ComputerName "DC02" -Credential $UserCredential
+
+Invoke-Command -Session $Session -ScriptBlock{    
+    Search-ADAccount -UsersOnly -LockedOut | Select-Object Name, SamAccountName, LastLogonDate | Format-Table
+    Search-ADAccount -UsersOnly -Lockedout | Unlock-AdAccount
+    Write-Host "Benutzer wurden entsperrt"
+}
+Remove-PSSession $Session
